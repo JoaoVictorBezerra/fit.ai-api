563776539af99113e2c35af3b64eb2002536f92a
João Victor <dev.joaovictor@hotmail.com>
2026-03-03 17:45:07 +0000

    chore: add simple import format

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,9 @@
 import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
@@ -11,4 +13,14 @@ export default defineConfig([
     languageOptions: { globals: globals.node },
   },
   tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
 ]);
